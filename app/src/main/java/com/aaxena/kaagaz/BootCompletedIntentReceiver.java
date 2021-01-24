a207fbe77c44af43b34365ee3f033a5c1e79b073
Spandan Saxena <spandansaxena@gmail.com>
2021-01-24 17:39:12 +0530

    Removed Internet and Storage Perms

--- app/src/main/java/com/aaxena/kaagaz/BootCompletedIntentReceiver.java
+++ app/src/main/java/com/aaxena/kaagaz/BootCompletedIntentReceiver.java
@@ -7,9 +7,7 @@ import android.content.Intent;
 public class BootCompletedIntentReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-            Intent pushIntent = new Intent(context, MainActivity.class);
+            Intent pushIntent = new Intent(context, AlarmService.class);
             context.startService(pushIntent);
         }
-    }
 }
